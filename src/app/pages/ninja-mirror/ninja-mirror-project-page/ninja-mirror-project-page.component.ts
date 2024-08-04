741d80b29c2957d7f25d509c910d76863ba73eb1
Damian Kumór <komura92@gmail.com>
2024-08-04 11:52:35 +0200

    # navigation auto active position

--- src/app/pages/ninja-mirror/ninja-mirror-project-page/ninja-mirror-project-page.component.ts
+++ src/app/pages/ninja-mirror/ninja-mirror-project-page/ninja-mirror-project-page.component.ts
@@ -1,41 +1,52 @@
-import {Component} from '@angular/core';
-import {NavigationEnd, Router} from "@angular/router";
+import {Component, OnInit, Renderer2} from '@angular/core';
 
 @Component({
-  selector: 'app-ninja-mirror-project-page',
-  standalone: false,
-  templateUrl: './ninja-mirror-project-page.component.html',
-  styleUrl: './ninja-mirror-project-page.component.scss'
+    selector: 'app-ninja-mirror-project-page',
+    standalone: false,
+    templateUrl: './ninja-mirror-project-page.component.html',
+    styleUrl: './ninja-mirror-project-page.component.scss'
 })
-export class NinjaMirrorProjectPageComponent {
+export class NinjaMirrorProjectPageComponent implements OnInit {
 
-  isOpen = false
+    isOpen = false
 
-  activeMenuOption : any
-  menuOptions = [
-    { translation: 'ninja.mirror.general.design.title', href: '#general-design'},
-    { translation: 'ninja.mirror.final.effect.title', href: '#final-effect'},
-    { translation: 'ninja.mirror.software.title', href: '#software'},
-    { translation: 'ninja.mirror.recommendation.title', href: '#recommendation'},
-    { translation: 'ninja.mirror.hardware.title', href: '#hardware'},
-    { translation: 'ninja.mirror.gallery.title', href: '#gallery'}
-  ]
+    sectionsOffsets: number[] = []
+    activeMenuOption: any
+    menuOptions = [
+        {translation: 'ninja.mirror.general.design.title', href: '#general-design'},
+        {translation: 'ninja.mirror.final.effect.title', href: '#final-effect'},
+        {translation: 'ninja.mirror.software.title', href: '#software'},
+        {translation: 'ninja.mirror.recommendation.title', href: '#recommendation'},
+        {translation: 'ninja.mirror.hardware.title', href: '#hardware'},
+        {translation: 'ninja.mirror.gallery.title', href: '#gallery'}
+    ]
 
 
-  constructor(private router : Router) {
-    router.events.subscribe((val) => {
-      if (val instanceof NavigationEnd) {
-        this.activeMenuOption = false;
-        let actualActiveOptionDivId = val.urlAfterRedirects.split('#')[1]
-        if (actualActiveOptionDivId) {
-          this.activeMenuOption = this.menuOptions
-            .find(menuOption => menuOption.href === '#' + actualActiveOptionDivId)
-        }
-      }
-    });
-  }
+    constructor(private renderer: Renderer2) {
+    }
 
-  toggleMenu() {
-    this.isOpen = !this.isOpen;
-  }
+    toggleMenu() {
+        this.isOpen = !this.isOpen;
+    }
+
+    ngOnInit(): void {
+        setTimeout(() => {
+            this.sectionsOffsets = this.menuOptions
+                .map(menuOption => document.getElementById(menuOption.href.substring(1))?.offsetTop!);
+
+            this.renderer.listen(window, 'scroll', ($event) => {
+                let firstItemOverNavbar = -1;
+                for (let i = 0; i < this.sectionsOffsets.length; i++) {
+                    if (window.scrollY >= this.sectionsOffsets[i] - 2)
+                        firstItemOverNavbar = i;
+                    else
+                      break;
+                }
+                if (firstItemOverNavbar >= 0)
+                  this.activeMenuOption = this.menuOptions[firstItemOverNavbar];
+                else
+                  this.activeMenuOption = null
+            })
+        }, 200)
+    }
 }
