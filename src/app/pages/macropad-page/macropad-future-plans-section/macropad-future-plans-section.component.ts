bd891c23b3f697b19211baa49c9df0f3755ad6dd
Damian Kumór <komura92@gmail.com>
2024-09-16 00:19:14 +0200

    # macropad future plans and repositories templates

--- src/app/pages/macropad-page/macropad-future-plans-section/macropad-future-plans-section.component.ts
+++ src/app/pages/macropad-page/macropad-future-plans-section/macropad-future-plans-section.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'macropad-future-plans-section',
   standalone: true,
-  imports: [],
+    imports: [
+        TranslateModule
+    ],
   templateUrl: './macropad-future-plans-section.component.html',
   styleUrl: './macropad-future-plans-section.component.css'
 })
--- src/app/pages/macropad-page/macropad-repositories-section/macropad-repositories-section.component.ts
+++ src/app/pages/macropad-page/macropad-repositories-section/macropad-repositories-section.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'macropad-repositories-section',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './macropad-repositories-section.component.html',
   styleUrl: './macropad-repositories-section.component.css'
 })
