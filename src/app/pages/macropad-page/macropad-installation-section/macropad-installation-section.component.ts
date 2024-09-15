15c8d0ac498db6781c130480c25e2d5916db7175
Damian Kumór <komura92@gmail.com>
2024-09-15 20:16:58 +0200

    # macropad general design and installation templates

--- src/app/pages/macropad-page/macropad-installation-section/macropad-installation-section.component.ts
+++ src/app/pages/macropad-page/macropad-installation-section/macropad-installation-section.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'macropad-installation-section',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './macropad-installation-section.component.html',
   styleUrl: './macropad-installation-section.component.css'
 })
