631735bbb7c37fe31ef416bc7f706358be044fef
Damian Kumór <komura92@gmail.com>
2024-09-16 23:03:25 +0200

    # design image per language

--- src/app/pages/macropad-page/macropad-general-design-section/macropad-general-design-section.component.ts
+++ src/app/pages/macropad-page/macropad-general-design-section/macropad-general-design-section.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'macropad-general-design-section',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './macropad-general-design-section.component.html',
   styleUrl: './macropad-general-design-section.component.css'
 })
